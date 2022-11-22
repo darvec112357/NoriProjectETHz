37f29b19a1afe4abcde49b885f741cde4e97991a
Jiugeng Sun <jiugengsun@student-net-nw-3872.intern.ethz.ch>
2022-11-22 20:20:50 +0100

    run bash update

--- run.sh
+++ run.sh
@@ -1,10 +1,4 @@
 git pull
-mkdir build
 cd build
-cmake ..
 make -j 4
 sbatch --wrap="xvfb-run -a ./nori /scenes/project/project.xml"
-cd ..
-git add .
-git commit -m "new render image result"
-git push 
