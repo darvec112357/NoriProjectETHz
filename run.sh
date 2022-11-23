1661ff87db587bb516dfed6b03db7d4294f16446
Jiugeng Sun <jiugengsun@student-net-nw-3872.intern.ethz.ch>
2022-11-23 13:26:46 +0100

    git ignore

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 git pull
 cd build
 make -j 4
-sbatch --men-per-cpu 16000 --wrap="xvfb-run -a ./nori ../scenes/project/project.xml"
+sbatch -c 128  --wrap="xvfb-run -a ./nori ../scenes/project/project.xml"
