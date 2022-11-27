74096840b3de06a2d8c0846e4cbbb1843691eba1
darvec112357 <rongxingtianxia611@163.com>
2022-11-27 20:06:28 +0100

    asd

--- src/spotlight.cpp
+++ src/spotlight.cpp
@@ -31,8 +31,8 @@ public:
         origin = props.getPoint3("origin");
         lookat = props.getPoint3("lookat");
         direction = Vector3f(lookat.x() - origin.x(), lookat.y() - origin.y(), lookat.z() - origin.z());
-        cosFalloffStart = cos(FalloffStart * M_PI / 180);
-        cosTotalWidth = cos(TotalWidth * M_PI / 180);
+        cosFalloffStart = std::cos(FalloffStart * M_PI / 180);
+        cosTotalWidth = std::cos(TotalWidth * M_PI / 180);
         //std::cout << cosFalloffStart << std::endl;
         //std::cout << cosTotalWidth << std::endl;
     }
@@ -42,7 +42,9 @@ public:
     }
 
     virtual Color3f eval(const EmitterQueryRecord & lRec) const override {
-        return power / 2 * (1 - 0.5 * (cosFalloffStart + cosTotalWidth));
+        float pdfVal = Warp::squareToUniformSphereCapPdf(Vector3f(0, 0, 1), cosTotalWidth);
+        float cosThetaLight = abs(lRec.n.dot(-lRec.wi));
+        return pdfVal / cosThetaLight;
     }
 
     virtual Color3f sample(EmitterQueryRecord & lRec, const Point2f & sample) const override {
@@ -52,10 +54,7 @@ public:
         lRec.n = direction.normalized();
 		float distance = (origin - ref).norm();
 		lRec.shadowRay = Ray3f(lRec.ref, lRec.wi, Epsilon, distance - Epsilon);
-        float costheta = lRec.n.dot(-lRec.wi);
-        Color3f intensity = power / (4 * M_PI);
-       
-        return intensity*fallout(costheta);
+        return eval(lRec) / (pdf(lRec));
     }
 
     virtual float pdf(const EmitterQueryRecord &lRec) const override {        
@@ -64,11 +63,11 @@ public:
 
     virtual float fallout(float costheta) const{
         if (costheta > cosFalloffStart) return 1;
-        if (costheta < cosTotalWidth) return 0;
-        float TotalWidth_r = TotalWidth * M_PI / 180;
-        float FalloffStart_r = FalloffStart * M_PI / 180;
-        float m_invTransitionWidth = 1 ;
-        return (TotalWidth_r - std::acos(costheta)) / (TotalWidth_r - FalloffStart_r);
+        else if (costheta < cosTotalWidth) return 0;
+        else {
+            float delta = (costheta - cosTotalWidth) / (cosFalloffStart - cosTotalWidth);
+            return delta * delta * delta * delta;
+        }
     }
 
 
